da63de256b5c19e8bade7517749416c3ddc87181
thedanisaur <definisdan@yahoo.com>
2023-01-18 21:35:01 -0500

    Add CORS

--- main.go
+++ main.go
@@ -6,12 +6,19 @@ import (
 	"user_service/handlers"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	log.Println("Starting User Auth...")
 	app := fiber.New()
 	defer db.GetInstance().Close()
+
+	// Add CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	// Basic Authentication
 	app.Post("/login", handlers.Login)
